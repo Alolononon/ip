b49682f59b43a2add0d83073726aa89a65e7e8cf
Alolononon <142967863+Alolononon@users.noreply.github.com>
2025-01-24 15:11:12 +0800

    Echo

--- src/main/java/Princess.java
+++ src/main/java/Princess.java
@@ -1,11 +1,32 @@
+import java.util.*;
+
 public class Princess {
   public static void main(String[] args) {
-    System.out.println("____________________________________________________________");
-    System.out.println("Hello! I'm Princess");
-    System.out.println("What can I do for you?");
-    System.out.println("____________________________________________________________");
-    System.out.println("Bye. Hope to see you again soon!");
-    System.out.println("____________________________________________________________");
+
+    Scanner sc = new Scanner(System.in);
+
+
+    System.out.println("    ____________________________________________________________");
+    System.out.println("    Hello! I'm Princess");
+    System.out.println("    What can I do for you?");
+    System.out.println("    ____________________________________________________________");
+    System.out.println();
+
+    String input = "";
+    while (true) {
+      input = sc.nextLine();
+      if (input.equals("bye"))
+        break;
+      System.out.println("    ____________________________________________________________");
+      System.out.println("    " + input);
+      System.out.println("    ____________________________________________________________");
+      System.out.println();
+    }
+
+
+    System.out.println("    ____________________________________________________________");
+    System.out.println("    Bye. Hope to see you again soon!");
+    System.out.println("    ____________________________________________________________");
 
 
 
