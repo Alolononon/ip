cc75db60dc6f78e624d7011d51c3648b601f07a9
Alolononon <142967863+Alolononon@users.noreply.github.com>
2025-01-24 15:28:05 +0800

    Add, List

--- src/main/java/Princess.java
+++ src/main/java/Princess.java
@@ -4,10 +4,12 @@ public class Princess {
   public static void main(String[] args) {
 
     Scanner sc = new Scanner(System.in);
+    ArrayList<String> storage = new ArrayList<>();
+
 
 
     System.out.println("    ____________________________________________________________");
-    System.out.println("    Hello! I'm Princess");
+    System.out.println("    Hello! I'm your Beautiful Princess");
     System.out.println("    What can I do for you?");
     System.out.println("    ____________________________________________________________");
     System.out.println();
@@ -15,10 +17,24 @@ public class Princess {
     String input = "";
     while (true) {
       input = sc.nextLine();
-      if (input.equals("bye"))
+      if (input.equals("bye"))    //exit loop
         break;
+
       System.out.println("    ____________________________________________________________");
-      System.out.println("    " + input);
+
+      if (input.equals("list")) {     //listing out the storage
+        int num = 1;
+        for (String elem : storage) {
+          System.out.println("     " + Integer.toString(num++) + ". " + elem);
+          
+        }
+      }
+      else {          //adding  new item into storage
+        storage.add(input);
+        System.out.println("     added: " + input);
+
+      }
+      
       System.out.println("    ____________________________________________________________");
       System.out.println();
     }
