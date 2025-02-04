86a6a470bd2b81a06f7227703082472a043c900f
Alolononon <142967863+Alolononon@users.noreply.github.com>
2025-02-04 14:33:05 +0800

    Add JavaDoc comments

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,25 +1,51 @@
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * Represents an event task with a start and end date/time.
+ */
 public class Event extends Task {
 
     protected LocalDateTime from;
     protected LocalDateTime to;
 
+    /**
+     * Creates an event task with a description, start date/time, and end date/time.
+     *
+     * @param description the task description
+     * @param from        the start date/time as a string
+     * @param to          the end date/time as a string
+     */
     public Event(String description, String from, String to) {
         super(description);
         this.from = DateParser.parseDateTime(from);
         this.to = DateParser.parseDateTime(to);
     }
 
+    /**
+     * Returns the formatted start date/time.
+     *
+     * @return the start date/time as a formatted string
+     */
     public String getFrom() {
         return from.format(DateTimeFormatter.ofPattern("MMM dd yyyy, h:mma"));
     }
 
+    /**
+     * Returns the formatted end date/time.
+     *
+     * @return the end date/time as a formatted string
+     */
     public String getTo() {
         return to.format(DateTimeFormatter.ofPattern("MMM dd yyyy, h:mma"));
     }
 
+
+    /**
+     * Returns the string representation of the event task.
+     *
+     * @return the formatted event task string
+     */
     @Override
     public String toString() {
         return "[E]" + super.toString() +
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,30 +1,62 @@
+
+/**
+ * Represents a task with a name and a completion status.
+ */
 public class Task {
-  private String taskName;
-  private boolean isDone;
+    private String taskName;
+    private boolean isDone;
 
-  public Task(String taskName) {
-    this.taskName = taskName;
-    this.isDone = false;
-  }
+    /**
+     * Constructs a new Task with the specified name.
+     * By default, the task is not completed.
+     *
+     * @param taskName The name or description of the task.
+     */
+    public Task(String taskName) {
+        this.taskName = taskName;
+        this.isDone = false;
+    }
 
-  public String getTaskName() {
-    return this.taskName;
-  }
-  
-  public boolean getIsTaskDone() {
-    return this.isDone;
-  }
+    /**
+     * Gets the name or description of the task.
+     *
+     * @return The task name.
+     */
+    public String getTaskName() {
+        return this.taskName;
+    }
 
-  public void markTask() {
-    this.isDone = true;
-  }
+    /**
+     * Checks if the task is completed.
+     *
+     * @return {@code true} if the task is done, otherwise {@code false}.
+     */
+    public boolean getIsTaskDone() {
+        return this.isDone;
+    }
 
-  public void unmarkTask() {
-    this.isDone = false;
-  }
+    /**
+     * Marks the task as completed.
+     */
+    public void markTask() {
+        this.isDone = true;
+    }
 
-  public String toString() {
-    return "[" + (this.isDone ? "X" : " ") + "] " + this.taskName;
-  }
+    /**
+     * Marks the task as not completed.
+     */
+    public void unmarkTask() {
+        this.isDone = false;
+    }
+
+    /**
+     * Returns the string representation of the task,
+     * indicating its completion status and name.
+     *
+     * @return A formatted string representing the task.
+     */
+    public String toString() {
+        return "[" + (this.isDone ? "X" : " ") + "] " + this.taskName;
+    }
 
 }
